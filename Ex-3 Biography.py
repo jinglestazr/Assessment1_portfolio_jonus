ac4d338508c542c4bc73a4bb899c98db7df04381
Jonus Thomas Philips <jonusthomas2@gmail.com>
2024-11-13 14:40:12 +0400

    edited

--- Ex-3 Biography.py
+++ Ex-3 Biography.py
@@ -1,6 +1,8 @@
 # create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
 
-info={'Name':'Jonus Thomas', 'Age':18, 'Hometown':'India'} # storing the information (name, hometown, and age) as key-value pairs in a dictionary.
+info={'Name':'Jonus Thomas', 
+      'Age':18, 
+      'Hometown':'India'} # storing the information (name, hometown, and age) as key-value pairs in a dictionary.
 
 print(info['Name'],info['Age'],info['Hometown'],sep='\n') # Print the values on separate lines using a single print statement.
 
